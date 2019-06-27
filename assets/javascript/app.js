1a477f5b84c679d9dc5fe586f4897bd8b1670ba2
Jesselyn Jackson <jesselyn.jackson@gm.com>
2019-06-27 14:17:17 -0400

    Display question and answers, create and display countdown timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,30 @@
-var qAndA = {
+let question = $("#question");
+let answer = $("#answer");
+let timer = $("#timer");
+let results = $("#results");
+let intervalId;
+let time = 10;
+let qAndA = {
     q1:{
         question: "How many letters are in the American alphabet?",
-        answer: "26"
+        answer: "26",
+        wrong1: "15",
+        wrong2: "30",
+        wrong3: "45"
     },
     q2:{
         question: "How many months are in a year?",
-        answer: "12"
+        answer: "12",
+        wrong1: "15",
+        wrong2: "10",
+        wrong3: "24"
     },
     q3:{
         question: "How many weeks are in a year?",
-        answer: "52"
+        answer: "52",
+        wrong1: "7",
+        wrong2: "365",
+        wrong3: "63"
     }
 };
 
@@ -19,4 +34,27 @@ console.log("First question: ", qAndA.q1.question);
 $("#start-btn").click(function () {
     $("#start-btn").hide();
     $("#gameField").show();
+    countdown();
 });
+
+//make sure time is displayed.
+timer.text(time);
+//make sure questions are displayed
+question.text(qAndA.q1.question);
+
+//make sure answers are displayed
+answer.append("<p>" + qAndA.q1.answer + "</p>").append("<p>" + qAndA.q1.wrong1 + "</p>").append("<p>" + qAndA.q1.wrong2 + "</p>").append("<p>" + qAndA.q1.wrong3 + "</p>");
+
+function countdown() {
+    intervalId = setInterval(timeRemaining, 1000);
+
+}
+
+function timeRemaining(){
+    time--;
+    timer.text(time);
+
+    if(time === 0){
+        clearInterval(intervalId);
+    }
+}
