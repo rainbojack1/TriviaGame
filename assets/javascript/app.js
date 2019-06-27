1f3196b03c6d5e99a099023bc6b1bbc0ccb99421
Jesselyn Jackson <jesselyn.jackson@gm.com>
2019-06-27 12:29:52 -0400

    Added a background, started the basic layout, and began an object of questions.

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+var qAndA = {
+    q1:{
+        question: "How many letters are in the American alphabet?",
+        answer: "26"
+    },
+    q2:{
+        question: "How many months are in a year?",
+        answer: "12"
+    },
+    q3:{
+        question: "How many weeks are in a year?",
+        answer: "52"
+    }
+};
+
+console.log("qAndA object: ", qAndA);
+console.log("First question: ", qAndA.q1.question);
+
+/*$("#start-btn").click(function () {
+    $("#start-btn").hide();
+});*/
