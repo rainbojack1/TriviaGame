778db96a777868f2564a56aa2d8c616c9328ba5d
Rainbo <jesselyn.jackson@gmail.com>
2019-06-29 19:24:54 -0400

    Added a function to dynamically populate question. Still need to work on switching to the next question.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,7 +5,8 @@ let results = $("#results");
 let intervalId;
 let time = 10;
 let answerArr = [];
-let scrambled = new Set();
+//let scrambled = new Set();
+let n = 1;
 let qAndA = {
     q1:{
         question: "How many letters are in the American alphabet?",
@@ -38,18 +39,25 @@ console.log("First question: ", qAndA.q1.question);
 $("#start-btn").click(function () {
     $("#start-btn").hide();
     $("#gameField").show();
-    countdown();
+    //countdown();
+    startGame();
 });
 
 //make sure time is displayed.
 timer.text(time);
 //make sure questions are displayed
-question.text(qAndA.q1.question);
+// question.text(qAndA.q1.question);
 
-//make sure answers are displayed
-//answer.append("<p><a href=''>" + qAndA.q1.answer + "</a></p>").append("<p><a href=''>" + qAndA.q1.wrong1 + "</a></p>").append("<p><a href=''>" + qAndA.q1.wrong2 + "</a></p>").append("<p><a href=''>" + qAndA.q1.wrong3 + "</a></p>");
+function startGame(){
+    countdown();
+    timeRemaining();
+    scrambleAnswers();
+    displayQuestion(n);
+    displayAnswers();
+}
 
 function countdown() {
+    clearInterval(intervalId);
     intervalId = setInterval(timeRemaining, 1000);
 
 }
@@ -73,40 +81,24 @@ function scrambleAnswers(){
         }
         
     }
-    console.log("answerArr: ", answerArr);
-
-    //create a random number to be the index of the item in answerArr to push to the scrambled set
-    //use a while loop to fill scrambled up to answerArr length
-    //push the array[scrambled index] to the set until the set has all four
-    
-    /*do {
-        let randomNum = Math.floor(Math.random() * 4);
-        scrambled.add(answerArr[randomNum]);
-        console.log("random num: ", randomNum);
-        console.log("Scrambled set size: ", scrambled.size);
-        if(scrambled.size == 4){
-            break;
-        }
-      }
-      while (scrambled.size < answerArr.length + 1);
-
-    console.log("Scrambled set: ", scrambled);*/
-
-      //let newArray = [1, 2, 3, 4];
-    /*for(var y = 0; y < answerArr.length; y++){
-        let randomNum = Math.floor(Math.random() * 4);
-        //$("#answer").append("<p><a href='' data-value=" + answerArr[randomNum] + ">" + answerArr[randomNum] + "</a><p>");
-        console.log("randomNum: ", randomNum);
-        console.log("newArray[randomNum]: ", newArray[randomNum]);
-        newArray.slice(0, 1);
-        console.log("newArray in for loop", newArray);
-    }*/
-
-    //return scrambled;
+    console.log("answerArr 1st: ", answerArr);
         
 }
 
-function displayAnswers(scrambled){
+function displayQuestion(qNum){
+    
+    if(qNum === 1){
+        $("#question").text(qAndA.q1.question);
+    }else if(qNum === 2){
+        $("#question").text(qAndA.q2.question);
+    }else if(qNum === 3){
+        $("#question").text(qAndA.q3.question);
+    }
+
+    displayAnswers();
+}
+
+function displayAnswers(){
    //create a new <p> and display the values of the scrambled set
     /*function populateAnswerList(scram){
         $("#answer").append("<p><a href='' class='choice' data-val='" + scram + "'>" + scram + "</a><p>");
@@ -114,12 +106,11 @@ function displayAnswers(scrambled){
     }
     scrambled.forEach(populateAnswerList);*/
 
-    console.log("answerArr is ", answerArr);
     let secondArray = [];
     for(var y = answerArr.length -1; y >= 0; y--){
         let randomNum = Math.floor(Math.random() * y);
         console.log("Pushing value: " + answerArr[randomNum] + " from index: " + randomNum);
-        secondArray.push(answerArr[randomNum])
+        secondArray.push(answerArr[randomNum]);
         console.log(answerArr);
         answerArr.splice(randomNum, 1);
         console.log(answerArr);
@@ -138,12 +129,20 @@ function checkAnswer(){
         if(($(this).data('val').toString() === qAndA.q1.answer)){
             console.log("Correct!");
         }
+        else{
+            console.log("Wrong!");
+        }
+        n++;
+        console.log("n= ", n);
+        //displayQuestion(n);
     })
     
 }
 
-$("#start-btn").click(function(){
-    displayAnswers(scrambled);
-})
+/*$("#start-btn").click(function(){
+    //displayAnswers();
+    //displayQuestion(n);
+    startGame();
+})*/
 
-scrambleAnswers();
+//scrambleAnswers();
